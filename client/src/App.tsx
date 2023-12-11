75071312bbc9ce6fac7096e19eeb560282ae9b36
hassan shahid <hassanshahid234@gmail.com>
2023-12-11 10:01:30 +0500

    header added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import SignIn from "./pages/SignIn"
+import SignOut from "./pages/SignOut"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
+import Header from "./components/Header"
+
 
 
 const App = () => {
   return (
-    <h1 className='text-red-500'>App</h1>
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-out" element={<SignOut/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
