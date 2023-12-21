f968ab48833a185549f073a492b3b1dc91c4be4e
hassan shahid <hassanshahid234@gmail.com>
2023-12-21 17:34:13 +0500

    create listing UI completed

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -224,7 +224,7 @@ const Profile = () => {
         </button>
         <Link
           className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
-          to={"/create-listing"}
+          to="/create-listing"
         >
           Create Listing
         </Link>
