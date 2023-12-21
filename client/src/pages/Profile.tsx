dfc1ca4dafafb7a39f5f0b1888fb901a71ef3578
hassan shahid <hassanshahid234@gmail.com>
2023-12-21 11:24:12 +0500

    signout functionality added

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -14,6 +14,9 @@ import {
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
+  signOutUserFailure,
+  signOutUserStart,
+  signOutUserSuccess,
   updateUserFailure,
   updateUserStart,
   updateUserSuccess,
@@ -128,7 +131,20 @@ const Profile = () => {
     }
   };
 
-  const handleSignOut = () => {};
+  const handleSignOut = async() => {
+    try {
+      dispatch(signOutUserStart())
+      const res = await fetch('/api/auth/signout')
+      const data = await res.json()
+      if(data.success === false){
+        dispatch(signOutUserFailure(data.message))
+        return
+      }
+      dispatch(signOutUserSuccess())
+    } catch (error) {
+      dispatch(signOutUserFailure(error.message))
+    }
+  };
 
   return (
     <div className="p-3 max-w-lg mx-auto">
