80ec8b3c1f3a702dc5c454990d7ac1c29a0d7b3d
hassan shahid <hassanshahid234@gmail.com>
2024-02-16 19:49:24 +0500

    search paage UI created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,6 +9,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
 import Listing from "./pages/Listing";
+import Search from "./pages/Search";
 
 const App = () => {
   return (
@@ -20,6 +21,8 @@ const App = () => {
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
         <Route path="/listing/:id" element={<Listing />} />
+        <Route path="/search" element={<Search />} />
+
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
