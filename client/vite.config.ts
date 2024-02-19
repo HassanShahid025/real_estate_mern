90bd42b442329c787310448f59a877890f8259cd
hassan shahid <hassanshahid234@gmail.com>
2024-02-19 12:06:59 +0500

    api added

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:'http://localhost:3000',
+        target:'https://real-estate-mern-4zdz.vercel.app',
         secure:false,
       }
     }
