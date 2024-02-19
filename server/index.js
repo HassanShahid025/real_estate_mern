f5cc145a885c9c9af52c73f51160ff232277255d
hassan shahid <hassanshahid234@gmail.com>
2024-02-19 18:31:36 +0500

    commit

--- server/index.js
+++ server/index.js
@@ -11,7 +11,13 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://real-estate-mern-client.vercel.app",
+    optionsSuccessStatus: 200,
+    preflightContinue: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
