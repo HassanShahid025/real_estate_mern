fcdce09ce75f75a31da7969d738d9fd6575aa43c
hassan shahid <hassanshahid234@gmail.com>
2024-02-19 12:20:25 +0500

    commit

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     proxy:{
       '/api':{
         target:'https://real-estate-mern-4zdz.vercel.app',
-        secure:false,
+        secure:true,
       }
     }
   },
