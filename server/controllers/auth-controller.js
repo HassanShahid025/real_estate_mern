735d692ff4fe825c2edbe9704a8223e22d7733c8
hassan shahid <hassanshahid234@gmail.com>
2024-02-23 11:26:20 +0500

    commit

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -30,6 +30,8 @@ export const signin = async (req, res, next) => {
     res
       .cookie("access_token", token, {
         httpOnly: true,
+        domain: "https://real-estate-mern-client.vercel.app",
+        secure: true,
       })
       .status(200)
       .json(rest);
--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ app.use(
     origin: "https://real-estate-mern-client.vercel.app",
     methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true,
+    
   })
 );
 app.use(express.json());
